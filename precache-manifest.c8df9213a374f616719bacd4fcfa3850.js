8073df82acf9524d5d18a63e238ab3d8747c7a26
dorozhkin-pv <dorozhkin.pv@yandex.ru>
2020-09-05 22:31:14 +0300

    Updates

---
+++ precache-manifest.c8df9213a374f616719bacd4fcfa3850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17f7a83dfe5f53133fce2b3921b840f",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "236685cdd96b6c78d10f",
+    "url": "/swapi/static/css/main.a1fa369a.chunk.css"
+  },
+  {
+    "revision": "9bee321dd9bad8bdd9a0",
+    "url": "/swapi/static/js/2.acf0ffcd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/swapi/static/js/2.acf0ffcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236685cdd96b6c78d10f",
+    "url": "/swapi/static/js/main.83e274e4.chunk.js"
+  },
+  {
+    "revision": "541955dd686d7b6ec0c0",
+    "url": "/swapi/static/js/runtime-main.aa1e55f8.js"
+  },
+  {
+    "revision": "6f01114719d69d19f3ee0e9a6be5d5b6",
+    "url": "/swapi/static/media/star-ship.6f011147.jpg"
+  }
+]);
